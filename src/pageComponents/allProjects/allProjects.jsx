1757023c7632203d49b57a5fc7a57a44952aeaa2
amir arab <amir4rab@github.com>
2021-07-21 20:40:21 +0430

    bug fixes

--- src/pageComponents/allProjects/allProjects.jsx
+++ src/pageComponents/allProjects/allProjects.jsx
@@ -73,7 +73,7 @@ const AllProjectsComponent = ({ location }) => {
             }
         }
     `);
-    const [ activeFilters, setActiveFilters] = useState( location.state === null ? [] : [location.state.preSelectedTag]);
+    const [ activeFilters, setActiveFilters] = useState( location.state === undefined || location.state === null ? [] : [location.state.preSelectedTag]);
 
     const [ tilteRef, titleInView ] = useInView(intersectionObserverSettings);
 
