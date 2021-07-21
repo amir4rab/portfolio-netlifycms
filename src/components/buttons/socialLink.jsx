5d79f9f4892b7147695e05f858fd5c632311dd32
amir arab <amir4rab@github.com>
2021-07-21 17:43:55 +0430

    socialLink bugfixes

--- src/components/buttons/socialLink.jsx
+++ src/components/buttons/socialLink.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function SocialLink({ href, className, children }) {
     return (
         <div className={className}>
-            <a href={ href } target="_">
+            <a href={ href } target="_blank" rel='noreferrer'>
                 { children }
             </a>
         </div>
