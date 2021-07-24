7b8c8b2126c16ab70c893408086818b774f8e2b6
amir arab <amir4rab@github.com>
2021-07-24 14:58:04 +0430

    added mobile layouts

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //** importing components **//
 import { Link } from "gatsby";
+import { motion } from 'framer-motion';
+
+//** importing images **//
+import mobileMenu from '../images/icons/colored/mobileMenu.svg'
 
 //** importing styles **//
 import "@fontsource/gothic-a1";
@@ -13,8 +17,42 @@ import "./layout.scss"
 //** importing components **/
 import IconLink from '../components/iconLink/iconLink';
 
+const mobileOverlayVariants = {
+    visible: {
+        left: 0,
+        opacity: 1,
+        transition: {
+            duration: .3
+        }
+    },
+    hidden: {
+        left: '-100%',
+        opacity: 0,
+        transition: {
+            duration: .3
+        }
+    }
+}
+
+const overlayLinksVariants = {
+    visible: i => ({
+        y: 0,
+        opacity: 1,
+        transition: {
+            delay: (i * .15) + .3,
+            duration: .15
+        }
+    }),
+    hidden: {
+        y: -10,
+        opacity: 0,
+    }
+}
+
 
 const Layout = ({ children }) => {
+    const [ mobileOverlay, setMobileOverlay ] = useState(false);
+
     return (
         <main className='layout'>
             <nav className="navbar">
@@ -39,7 +77,82 @@ const Layout = ({ children }) => {
                     </ul>
                 </li>
             </nav>
+            <motion.div className='mobileOverlay'
+                variants={ mobileOverlayVariants }
+                animate={ mobileOverlay ? 'visible' : 'hidden' }
+                initial='hidden'
+            >
+                <div className="inner">
+                    <motion.button
+                        variants={ overlayLinksVariants }
+                        custom={ 0 }
+                        initial='hidden'
+                        animate={ mobileOverlay ? 'visible' : 'hidden' }
+                        className='link'
+                        onClick={ _ => setMobileOverlay(!mobileOverlay) }
+                    >
+                        <Link activeClassName="activeLink" to="/">Home</Link>
+                    </motion.button>
+                    <motion.button
+                        variants={ overlayLinksVariants }
+                        custom={ 1 }
+                        initial='hidden'
+                        animate={ mobileOverlay ? 'visible' : 'hidden' }
+                        className='link'
+                        onClick={ _ => setMobileOverlay(!mobileOverlay) }
+                    >
+                        <Link activeClassName="activeLink" to="/about">About</Link>
+                    </motion.button>
+                    <motion.button
+                        variants={ overlayLinksVariants }
+                        custom={ 2 }
+                        initial='hidden'
+                        animate={ mobileOverlay ? 'visible' : 'hidden' }
+                        className='link'
+                        onClick={ _ => setMobileOverlay(!mobileOverlay) }
+                    >
+                        <Link activeClassName="activeLink" to="/projects">Projects</Link>
+                    </motion.button>
+                    <motion.button
+                        variants={ overlayLinksVariants }
+                        custom={ 3 }
+                        initial='hidden'
+                        animate={ mobileOverlay ? 'visible' : 'hidden' }
+                        className='link'
+                        onClick={ _ => setMobileOverlay(!mobileOverlay) }
+                    >
+                        <Link activeClassName="activeLink" to="/contact">Contact</Link>
+                    </motion.button>
+                    <motion.button
+                        variants={ overlayLinksVariants }
+                        custom={ 4 }
+                        initial='hidden'
+                        animate={ mobileOverlay ? 'visible' : 'hidden' }
+                        className='link'
+                        onClick={ _ => setMobileOverlay(!mobileOverlay) }
+                    >
+                        <Link activeClassName="activeLink" to="/blog">Blog</Link>
+                    </motion.button>
+                    <motion.button
+                        variants={ overlayLinksVariants }
+                        custom={ 4 }
+                        initial='hidden'
+                        animate={ mobileOverlay ? 'visible' : 'hidden' }
+                        className='exit'
+                        onClick={ _ => setMobileOverlay(!mobileOverlay) }
+                    >
+                        close
+                    </motion.button>
+                </div>
+            </motion.div>
             { children }
+            <div className="mobileBtn">
+                <button
+                    onClick={ _ => setMobileOverlay(!mobileOverlay) }
+                >
+                    <img src={ mobileMenu } alt="mobile menu icon" />
+                </button>
+            </div>
         </main>
     );
 };
